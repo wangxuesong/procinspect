c5ac0d8f449dcd9806368984d2aa085ac5895eed
Martin Wang <wangxuesong@gmail.com>
2023-04-07 15:12:51 +0800

    feat(parser): 添加语法树节点的行和列信息
    现在语法树节点可以存储它们在源代码中的行和列信息。这些信息可以通过新的Line()和Column()方法访问。此外，添加了SetLine()和SetColumn()方法，以便在创建节点时设置这些信息。这些更改使得在调试和错误报告中更容易定位源代码中的问题。
    
    Signed-off-by: Martin Wang <wangxuesong@gmail.com>

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -53,6 +53,8 @@ func TestParseSimple(t *testing.T) {
 	stmt, ok := node.Statements[0].(*semantic.SelectStatement)
 	assert.True(t, ok)
 	assert.NotNil(t, stmt)
+	assert.Equal(t, 1, stmt.Line())
+	assert.Equal(t, 1, stmt.Column())
 	assert.Equal(t, len(stmt.Fields.Fields), 1)
 	assert.Equal(t, stmt.Fields.Fields[0].WildCard.Table, "*")
 	assert.Equal(t, len(stmt.From.TableRefs), 2)
@@ -73,6 +75,9 @@ func TestCreateProcedure(t *testing.T) {
 		assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
 		stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
 
+		assert.Equal(t, 1, stmt.Line())
+		assert.Equal(t, 1, stmt.Column())
+
 		assert.True(t, stmt.IsReplace)
 
 		assert.Equal(t, stmt.Name, "test")
@@ -83,10 +88,16 @@ func TestCreateProcedure(t *testing.T) {
 		assert.IsType(t, &semantic.SelectStatement{}, stmt.Body.Statements[0])
 		select1 := stmt.Body.Statements[0].(*semantic.SelectStatement)
 		assert.Equal(t, select1.From.TableRefs[0].Table, "dual")
+		// assert line & column
+		assert.Equal(t, 3, select1.Line())
+		assert.Equal(t, 3, select1.Column())
 
 		assert.IsType(t, &semantic.SelectStatement{}, stmt.Body.Statements[1])
 		select2 := stmt.Body.Statements[1].(*semantic.SelectStatement)
 		assert.Equal(t, select2.From.TableRefs[0].Table, "t")
+		// assert line & column
+		assert.Equal(t, 4, select2.Line())
+		assert.Equal(t, 3, select2.Column())
 	}
 
 	text = `CREATE OR REPLACE PROCEDURE PROC(PARAM NUMBER)
@@ -103,6 +114,10 @@ END;`
 		assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
 		stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
 
+		// assert line & column
+		assert.Equal(t, 1, stmt.Line())
+		assert.Equal(t, 1, stmt.Column())
+
 		assert.True(t, stmt.IsReplace)
 
 		assert.Equal(t, stmt.Name, "PROC")
@@ -113,14 +128,28 @@ END;`
 		assert.NotNil(t, stmt.Declarations)
 		assert.Equal(t, len(stmt.Declarations), 2)
 
-		assert.Equal(t, &semantic.VariableDeclaration{Name: "LOCAL_PARAM", DataType: "NUMBER"}, stmt.Declarations[0])
-		assert.Equal(t, &semantic.ExceptionDeclaration{Name: "USER_EXCEPTION"}, stmt.Declarations[1])
+		assert.IsType(t, &semantic.VariableDeclaration{Name: "LOCAL_PARAM", DataType: "NUMBER"}, stmt.Declarations[0])
+		// assert line & column
+		assert.Equal(t, 3, stmt.Declarations[0].Line())
+		assert.Equal(t, 1, stmt.Declarations[0].Column())
+		decl1 := stmt.Declarations[0].(*semantic.VariableDeclaration)
+		assert.Equal(t, decl1.Name, "LOCAL_PARAM")
+		assert.Equal(t, decl1.DataType, "NUMBER")
+		assert.IsType(t, &semantic.ExceptionDeclaration{Name: "USER_EXCEPTION"}, stmt.Declarations[1])
+		// assert line & column
+		assert.Equal(t, 4, stmt.Declarations[1].Line())
+		assert.Equal(t, 1, stmt.Declarations[1].Column())
+		decl2 := stmt.Declarations[1].(*semantic.ExceptionDeclaration)
+		assert.Equal(t, decl2.Name, "USER_EXCEPTION")
 
 		assert.NotNil(t, stmt.Body)
 		assert.Equal(t, len(stmt.Body.Statements), 1)
 
-		assert.Equal(t, &semantic.AssignmentStatement{Left: "LOCAL_PARAM", Right: "0"}, stmt.Body.Statements[0])
+		assert.IsType(t, &semantic.AssignmentStatement{}, stmt.Body.Statements[0])
 		stmt1 := stmt.Body.Statements[0].(*semantic.AssignmentStatement)
+		// assert line & column
+		assert.Equal(t, 6, stmt1.Line())
+		assert.Equal(t, 1, stmt1.Column())
 		assert.Equal(t, stmt1.Left, "LOCAL_PARAM")
 		assert.Equal(t, stmt1.Right, "0")
 	}
--- pkg/semantic/node.go
+++ pkg/semantic/node.go
@@ -5,9 +5,13 @@ type (
 
 	Node interface {
 		Type() NodeType
+		Line() int
+		Column() int
 	}
 
 	node struct {
+		line   int
+		column int
 	}
 
 	Script struct {
@@ -26,6 +30,22 @@ const (
 
 func (*node) Type() NodeType {
 	return NilNode
+}
+
+func (n *node) Line() int {
+	return n.line
+}
+
+func (n *node) Column() int {
+	return n.column
+}
+
+func (n *node) SetLine(line int) {
+	n.line = line
+}
+
+func (n *node) SetColumn(column int) {
+	n.column = column + 1
 }
 
 func (*Script) Type() NodeType {
