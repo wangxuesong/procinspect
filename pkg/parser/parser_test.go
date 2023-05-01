5c52d681a73d2a5c03e8766bf556353b40908afa
Martin Wang <wangxuesong@gmail.com>
2023-05-01 16:31:34 +0800

    重构(parser_test.go)：将测试用例拆分为多个函数并使用测试套件
    这个提交将测试用例拆分为多个函数，并使用测试套件来组织它们。添加了测试用例以测试不同类型的PL/SQL语句的解析，包括简单的SELECT语句、CREATE PROCEDURE语句和带有参数的CREATE PROCEDURE语句。这些测试用例确保了解析器能够正确地解析各种类型的PL/SQL语句。
    
    功能(parser_test.go)：使用runTestSuite函数简化测试用例的编写
    
    现在测试用例使用runTestSuite函数来运行测试，这样可以更容易地编写和维护测试用例。同时，将getRoot函数替换为runTestSuite函数，以避免重复代码。
    
    Signed-off-by: Martin Wang <wangxuesong@gmail.com>

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"procinspect/pkg/semantic"
 	"testing"
 
@@ -8,6 +9,45 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
+
+type (
+	testCase struct {
+		name string
+		text string
+		Func testCaseFunc
+	}
+
+	testCaseFunc func(*testing.T, *semantic.Script)
+
+	testSuite []testCase
+)
+
+func getRoot(t *testing.T, text string) *semantic.Script {
+	p := plsql.NewParser(text)
+	root := p.Sql_script()
+	assert.Nil(t, p.Error())
+	assert.NotNil(t, root)
+	assert.IsType(t, &plsql.Sql_scriptContext{}, root)
+	_, ok := root.(*plsql.Sql_scriptContext)
+	assert.True(t, ok)
+
+	node := GeneralScript(root)
+	assert.NotNil(t, node)
+	assert.Greater(t, len(node.Statements), 0)
+	return node
+}
+
+func runTest(t *testing.T, input string, testFunc func(t *testing.T, node *semantic.Script)) {
+	node := getRoot(t, input)
+	testFunc(t, node)
+}
+
+func runTestSuite(t *testing.T, tests testSuite) {
+	for _, test := range tests {
+		fmt.Println(test.name)
+		runTest(t, test.text, test.Func)
+	}
+}
 
 func TestParseCreatePackage(t *testing.T) {
 	text := `create or replace package test is
@@ -38,125 +78,135 @@ func TestParseCreateProc(t *testing.T) {
 }
 
 func TestParseSimple(t *testing.T) {
-	text := `select * from dual, test;`
+	tests := testSuite{}
 
-	p := plsql.NewParser(text)
-	root := p.Sql_script()
-	assert.Nil(t, p.Error())
-	assert.NotNil(t, root)
-	assert.IsType(t, &plsql.Sql_scriptContext{}, root)
-	_, ok := root.(*plsql.Sql_scriptContext)
-	assert.True(t, ok)
-	node := GeneralScript(root)
-	assert.NotNil(t, node)
-	assert.Greater(t, len(node.Statements), 0)
-	stmt, ok := node.Statements[0].(*semantic.SelectStatement)
-	assert.True(t, ok)
-	assert.NotNil(t, stmt)
-	assert.Equal(t, 1, stmt.Line())
-	assert.Equal(t, 1, stmt.Column())
-	assert.Equal(t, len(stmt.Fields.Fields), 1)
-	assert.Equal(t, stmt.Fields.Fields[0].WildCard.Table, "*")
-	assert.Equal(t, len(stmt.From.TableRefs), 2)
-	assert.Equal(t, stmt.From.TableRefs[0].Table, "dual")
-	assert.Equal(t, stmt.From.TableRefs[1].Table, "test")
+	tests = append(tests, testCase{
+		name: "simple",
+		text: `select * from dual, test;`,
+		Func: func(t *testing.T, node *semantic.Script) {
+			assert.Greater(t, len(node.Statements), 0)
+			stmt, ok := node.Statements[0].(*semantic.SelectStatement)
+			assert.True(t, ok)
+			assert.NotNil(t, stmt)
+			assert.Equal(t, 1, stmt.Line())
+			assert.Equal(t, 1, stmt.Column())
+			assert.Equal(t, len(stmt.Fields.Fields), 1)
+			assert.Equal(t, stmt.Fields.Fields[0].WildCard.Table, "*")
+			assert.Equal(t, len(stmt.From.TableRefs), 2)
+			assert.Equal(t, stmt.From.TableRefs[0].Table, "dual")
+			assert.Equal(t, stmt.From.TableRefs[1].Table, "test")
+
+		},
+	})
+
+	runTestSuite(t, tests)
 }
 
 func TestCreateProcedure(t *testing.T) {
-	text := `create or replace procedure test is
+	var tests = testSuite{}
+
+	tests = append(tests, testCase{
+		name: "create procedure",
+		text: `create or replace procedure test is
 	begin
 		select 1 from dual;
 		select 2 from t;
-	end;`
+	end;`,
+		Func: func(t *testing.T, node *semantic.Script) {
+			// assert that the statement is a CreateProcedureStatement
+			assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
+			stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
 
-	node := getRoot(t, text)
+			assert.Equal(t, 1, stmt.Line())
+			assert.Equal(t, 1, stmt.Column())
 
-	{ // assert that the statement is a CreateProcedureStatement
-		assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
-		stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
+			assert.True(t, stmt.IsReplace)
 
-		assert.Equal(t, 1, stmt.Line())
-		assert.Equal(t, 1, stmt.Column())
+			assert.Equal(t, stmt.Name, "test")
 
-		assert.True(t, stmt.IsReplace)
+			assert.NotNil(t, stmt.Body)
+			assert.Equal(t, len(stmt.Body.Statements), 2)
 
-		assert.Equal(t, stmt.Name, "test")
+			assert.IsType(t, &semantic.SelectStatement{}, stmt.Body.Statements[0])
+			select1 := stmt.Body.Statements[0].(*semantic.SelectStatement)
+			assert.Equal(t, select1.From.TableRefs[0].Table, "dual")
+			// assert line & column
+			assert.Equal(t, 3, select1.Line())
+			assert.Equal(t, 3, select1.Column())
 
-		assert.NotNil(t, stmt.Body)
-		assert.Equal(t, len(stmt.Body.Statements), 2)
+			assert.IsType(t, &semantic.SelectStatement{}, stmt.Body.Statements[1])
+			select2 := stmt.Body.Statements[1].(*semantic.SelectStatement)
+			assert.Equal(t, select2.From.TableRefs[0].Table, "t")
+			// assert line & column
+			assert.Equal(t, 4, select2.Line())
+			assert.Equal(t, 3, select2.Column())
+		},
+	})
 
-		assert.IsType(t, &semantic.SelectStatement{}, stmt.Body.Statements[0])
-		select1 := stmt.Body.Statements[0].(*semantic.SelectStatement)
-		assert.Equal(t, select1.From.TableRefs[0].Table, "dual")
-		// assert line & column
-		assert.Equal(t, 3, select1.Line())
-		assert.Equal(t, 3, select1.Column())
-
-		assert.IsType(t, &semantic.SelectStatement{}, stmt.Body.Statements[1])
-		select2 := stmt.Body.Statements[1].(*semantic.SelectStatement)
-		assert.Equal(t, select2.From.TableRefs[0].Table, "t")
-		// assert line & column
-		assert.Equal(t, 4, select2.Line())
-		assert.Equal(t, 3, select2.Column())
-	}
-
-	text = `CREATE OR REPLACE PROCEDURE PROC(PARAM NUMBER)
+	tests = append(tests, testCase{
+		name: "create procedure with parameters",
+		text: `CREATE OR REPLACE PROCEDURE PROC(PARAM NUMBER)
 IS
 LOCAL_PARAM NUMBER;
 USER_EXCEPTION EXCEPTION;
 BEGIN
 LOCAL_PARAM:=0;
-END;`
+END;`,
+		Func: func(t *testing.T, node *semantic.Script) {
+			// assert that the statement is a CreateProcedureStatement
+			assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
+			stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
 
-	node = getRoot(t, text)
+			// assert line & column
+			assert.Equal(t, 1, stmt.Line())
+			assert.Equal(t, 1, stmt.Column())
 
-	{ // assert that the statement is a CreateProcedureStatement
-		assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
-		stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
+			assert.True(t, stmt.IsReplace)
 
-		// assert line & column
-		assert.Equal(t, 1, stmt.Line())
-		assert.Equal(t, 1, stmt.Column())
+			assert.Equal(t, stmt.Name, "PROC")
 
-		assert.True(t, stmt.IsReplace)
+			assert.Equal(t, len(stmt.Parameters), 1)
+			assert.Equal(t, &semantic.Parameter{Name: "PARAM", DataType: "NUMBER"}, stmt.Parameters[0])
 
-		assert.Equal(t, stmt.Name, "PROC")
+			assert.NotNil(t, stmt.Declarations)
+			assert.Equal(t, len(stmt.Declarations), 2)
 
-		assert.Equal(t, len(stmt.Parameters), 1)
-		assert.Equal(t, &semantic.Parameter{Name: "PARAM", DataType: "NUMBER"}, stmt.Parameters[0])
+			assert.IsType(t, &semantic.VariableDeclaration{Name: "LOCAL_PARAM", DataType: "NUMBER"}, stmt.Declarations[0])
+			// assert line & column
+			assert.Equal(t, 3, stmt.Declarations[0].Line())
+			assert.Equal(t, 1, stmt.Declarations[0].Column())
+			decl1 := stmt.Declarations[0].(*semantic.VariableDeclaration)
+			assert.Equal(t, decl1.Name, "LOCAL_PARAM")
+			assert.Equal(t, decl1.DataType, "NUMBER")
+			assert.IsType(t, &semantic.ExceptionDeclaration{Name: "USER_EXCEPTION"}, stmt.Declarations[1])
+			// assert line & column
+			assert.Equal(t, 4, stmt.Declarations[1].Line())
+			assert.Equal(t, 1, stmt.Declarations[1].Column())
+			decl2 := stmt.Declarations[1].(*semantic.ExceptionDeclaration)
+			assert.Equal(t, decl2.Name, "USER_EXCEPTION")
 
-		assert.NotNil(t, stmt.Declarations)
-		assert.Equal(t, len(stmt.Declarations), 2)
+			assert.NotNil(t, stmt.Body)
+			assert.Equal(t, len(stmt.Body.Statements), 1)
 
-		assert.IsType(t, &semantic.VariableDeclaration{Name: "LOCAL_PARAM", DataType: "NUMBER"}, stmt.Declarations[0])
-		// assert line & column
-		assert.Equal(t, 3, stmt.Declarations[0].Line())
-		assert.Equal(t, 1, stmt.Declarations[0].Column())
-		decl1 := stmt.Declarations[0].(*semantic.VariableDeclaration)
-		assert.Equal(t, decl1.Name, "LOCAL_PARAM")
-		assert.Equal(t, decl1.DataType, "NUMBER")
-		assert.IsType(t, &semantic.ExceptionDeclaration{Name: "USER_EXCEPTION"}, stmt.Declarations[1])
-		// assert line & column
-		assert.Equal(t, 4, stmt.Declarations[1].Line())
-		assert.Equal(t, 1, stmt.Declarations[1].Column())
-		decl2 := stmt.Declarations[1].(*semantic.ExceptionDeclaration)
-		assert.Equal(t, decl2.Name, "USER_EXCEPTION")
+			assert.IsType(t, &semantic.AssignmentStatement{}, stmt.Body.Statements[0])
+			stmt1 := stmt.Body.Statements[0].(*semantic.AssignmentStatement)
+			// assert line & column
+			assert.Equal(t, 6, stmt1.Line())
+			assert.Equal(t, 1, stmt1.Column())
+			assert.Equal(t, stmt1.Left, "LOCAL_PARAM")
+			assert.Equal(t, stmt1.Right, "0")
+		},
+	})
 
-		assert.NotNil(t, stmt.Body)
-		assert.Equal(t, len(stmt.Body.Statements), 1)
-
-		assert.IsType(t, &semantic.AssignmentStatement{}, stmt.Body.Statements[0])
-		stmt1 := stmt.Body.Statements[0].(*semantic.AssignmentStatement)
-		// assert line & column
-		assert.Equal(t, 6, stmt1.Line())
-		assert.Equal(t, 1, stmt1.Column())
-		assert.Equal(t, stmt1.Left, "LOCAL_PARAM")
-		assert.Equal(t, stmt1.Right, "0")
-	}
+	runTestSuite(t, tests)
 }
 
 func TestCursorDeclaration(t *testing.T) {
-	text := `create or replace procedure test is
+	var tests testSuite
+
+	tests = append(tests, testCase{
+		name: "无参数游标声明",
+		text: `create or replace procedure test is
 	Cursor c_AllAws Is
 		Select *
 		From Asc_Work_Status
@@ -165,38 +215,39 @@ func TestCursorDeclaration(t *testing.T) {
 	Rec_AllAws c_AllAws%Rowtype;
 	begin
 		select 1 from dual;
-	end;`
+	end;`,
+		Func: func(t *testing.T, node *semantic.Script) {
+			// assert that the statement is a CreateProcedureStatement
+			assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
+			stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
+			assert.Equal(t, 1, stmt.Line())
+			assert.Equal(t, 1, stmt.Column())
+			assert.True(t, stmt.IsReplace)
+			assert.Equal(t, stmt.Name, "test")
 
-	node := getRoot(t, text)
+			// assert declaration
+			assert.NotNil(t, stmt.Declarations)
+			assert.Equal(t, len(stmt.Declarations), 2)
+			// assert the declaration is a CursorDeclaration
+			assert.IsType(t, &semantic.CursorDeclaration{}, stmt.Declarations[0])
+			decl := stmt.Declarations[0].(*semantic.CursorDeclaration)
+			assert.Equal(t, decl.Name, "c_AllAws")
+			// assert the parameters is nil
+			assert.Nil(t, decl.Parameters)
+			// assert the declaration is a VariableDeclaration
+			assert.IsType(t, &semantic.VariableDeclaration{}, stmt.Declarations[1])
+			vardecl := stmt.Declarations[1].(*semantic.VariableDeclaration)
+			assert.Equal(t, vardecl.Name, "Rec_AllAws")
+			assert.Equal(t, vardecl.DataType, "c_AllAws%Rowtype")
+			// assert the statement is a SelectStatement
+			assert.NotNil(t, decl.Stmt)
+			assert.IsType(t, &semantic.SelectStatement{}, decl.Stmt)
+		},
+	})
 
-	{ // assert that the statement is a CreateProcedureStatement
-		assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
-		stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
-		assert.Equal(t, 1, stmt.Line())
-		assert.Equal(t, 1, stmt.Column())
-		assert.True(t, stmt.IsReplace)
-		assert.Equal(t, stmt.Name, "test")
-
-		// assert declaration
-		assert.NotNil(t, stmt.Declarations)
-		assert.Equal(t, len(stmt.Declarations), 2)
-		// assert the declaration is a CursorDeclaration
-		assert.IsType(t, &semantic.CursorDeclaration{}, stmt.Declarations[0])
-		decl := stmt.Declarations[0].(*semantic.CursorDeclaration)
-		assert.Equal(t, decl.Name, "c_AllAws")
-		// assert the parameters is nil
-		assert.Nil(t, decl.Parameters)
-		// assert the declaration is a VariableDeclaration
-		assert.IsType(t, &semantic.VariableDeclaration{}, stmt.Declarations[1])
-		vardecl := stmt.Declarations[1].(*semantic.VariableDeclaration)
-		assert.Equal(t, vardecl.Name, "Rec_AllAws")
-		assert.Equal(t, vardecl.DataType, "c_AllAws%Rowtype")
-		// assert the statement is a SelectStatement
-		assert.NotNil(t, decl.Stmt)
-		assert.IsType(t, &semantic.SelectStatement{}, decl.Stmt)
-	}
-
-	text = `create or replace procedure test is
+	tests = append(tests, testCase{
+		name: "有参数游标声明",
+		text: `create or replace procedure test is
 	Cursor c_Aws(m_Areano Varchar2) Is
 		Select *
 		From Asc_Work_Status
@@ -205,54 +256,40 @@ func TestCursorDeclaration(t *testing.T) {
 		Rec_Aws c_Aws%Rowtype;
 	begin
 		select 1 from dual;
-	end;`
+	end;`,
+		Func: func(t *testing.T, node *semantic.Script) {
+			// assert that the statement is a CreateProcedureStatement
+			assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
+			stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
+			assert.Equal(t, 1, stmt.Line())
+			assert.Equal(t, 1, stmt.Column())
+			assert.True(t, stmt.IsReplace)
+			assert.Equal(t, stmt.Name, "test")
 
-	node = getRoot(t, text)
+			// assert declaration
+			assert.NotNil(t, stmt.Declarations)
+			assert.Equal(t, len(stmt.Declarations), 2)
+			// assert the declaration is a CursorDeclaration
+			assert.IsType(t, &semantic.CursorDeclaration{}, stmt.Declarations[0])
+			decl := stmt.Declarations[0].(*semantic.CursorDeclaration)
+			assert.Equal(t, decl.Name, "c_Aws")
+			// assert the parameters is not nil
+			assert.NotNil(t, decl.Parameters)
+			assert.Equal(t, len(decl.Parameters), 1)
+			// assert the parameter is a Parameter
+			param := decl.Parameters[0]
+			assert.Equal(t, param.Name, "m_Areano")
+			assert.Equal(t, param.DataType, "Varchar2")
+			// assert the declaration is a VariableDeclaration
+			assert.IsType(t, &semantic.VariableDeclaration{}, stmt.Declarations[1])
+			vardecl := stmt.Declarations[1].(*semantic.VariableDeclaration)
+			assert.Equal(t, vardecl.Name, "Rec_Aws")
+			assert.Equal(t, vardecl.DataType, "c_Aws%Rowtype")
+			// assert the statement is a SelectStatement
+			assert.NotNil(t, decl.Stmt)
+			assert.IsType(t, &semantic.SelectStatement{}, decl.Stmt)
+		},
+	})
 
-	{ // assert that the statement is a CreateProcedureStatement
-		assert.IsType(t, &semantic.CreateProcedureStatement{}, node.Statements[0])
-		stmt := node.Statements[0].(*semantic.CreateProcedureStatement)
-		assert.Equal(t, 1, stmt.Line())
-		assert.Equal(t, 1, stmt.Column())
-		assert.True(t, stmt.IsReplace)
-		assert.Equal(t, stmt.Name, "test")
-
-		// assert declaration
-		assert.NotNil(t, stmt.Declarations)
-		assert.Equal(t, len(stmt.Declarations), 2)
-		// assert the declaration is a CursorDeclaration
-		assert.IsType(t, &semantic.CursorDeclaration{}, stmt.Declarations[0])
-		decl := stmt.Declarations[0].(*semantic.CursorDeclaration)
-		assert.Equal(t, decl.Name, "c_Aws")
-		// assert the parameters is not nil
-		assert.NotNil(t, decl.Parameters)
-		assert.Equal(t, len(decl.Parameters), 1)
-		// assert the parameter is a Parameter
-		param := decl.Parameters[0]
-		assert.Equal(t, param.Name, "m_Areano")
-		assert.Equal(t, param.DataType, "Varchar2")
-		// assert the declaration is a VariableDeclaration
-		assert.IsType(t, &semantic.VariableDeclaration{}, stmt.Declarations[1])
-		vardecl := stmt.Declarations[1].(*semantic.VariableDeclaration)
-		assert.Equal(t, vardecl.Name, "Rec_Aws")
-		assert.Equal(t, vardecl.DataType, "c_Aws%Rowtype")
-		// assert the statement is a SelectStatement
-		assert.NotNil(t, decl.Stmt)
-		assert.IsType(t, &semantic.SelectStatement{}, decl.Stmt)
-	}
-}
-
-func getRoot(t *testing.T, text string) *semantic.Script {
-	p := plsql.NewParser(text)
-	root := p.Sql_script()
-	assert.Nil(t, p.Error())
-	assert.NotNil(t, root)
-	assert.IsType(t, &plsql.Sql_scriptContext{}, root)
-	_, ok := root.(*plsql.Sql_scriptContext)
-	assert.True(t, ok)
-
-	node := GeneralScript(root)
-	assert.NotNil(t, node)
-	assert.Greater(t, len(node.Statements), 0)
-	return node
+	runTestSuite(t, tests)
 }
