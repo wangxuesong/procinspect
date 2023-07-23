6526c9f2bad73ae20cdd5f2f3a3b06e748927a92
Martin Wang <wangxuesong@gmail.com>
2023-07-23 11:12:31 +0800

    feat(log): 完善日志库功能
    添加了一个新的config字段来存储zap.Config对象
    添加了DefaultLogger函数来创建默认的Logger对象，并将config字段初始化为默认的zap.Config对象
    添加了CurrentLogger函数来获取当前的Logger对象
    添加了Info、Warn、Error和Debug函数来记录不同级别的日志
    添加了Sync函数来同步日志
    添加了SetLevel函数来设置日志级别

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -12,6 +12,8 @@ type (
 	Logger struct {
 		l     *zap.Logger
 		level Level
+
+		config *zap.Config
 	}
 )
 
@@ -39,4 +41,48 @@ var (
 	Sync  = logger.Sync
 )
 
-var logger, _ = zap.NewProduction(zap.WithCaller(false), zap.AddStacktrace(ErrorLevel))
+var logger, _ = DefaultLogger()
+
+func DefaultLogger() (*Logger, error) {
+	cfg := zap.NewProductionConfig()
+	cfg.Sampling = nil
+	cfg.Level = zap.NewAtomicLevelAt(InfoLevel)
+	log, err := cfg.Build(zap.WithCaller(false), zap.AddStacktrace(ErrorLevel))
+	if err != nil {
+		return nil, err
+	}
+	return &Logger{l: log, level: InfoLevel, config: &cfg}, nil
+}
+
+func CurrentLogger() *Logger {
+	return logger
+}
+
+func (log *Logger) Info(msg string, fields ...Field) {
+	log.l.Info(msg, fields...)
+}
+
+func (log *Logger) Warn(msg string, fields ...Field) {
+	log.l.Warn(msg, fields...)
+}
+
+func (log *Logger) Error(msg string, fields ...Field) {
+	log.l.Error(msg, fields...)
+}
+
+func (log *Logger) Debug(msg string, fields ...Field) {
+	log.l.Debug(msg, fields...)
+}
+
+func (log *Logger) Sync() {
+	log.l.Sync()
+}
+
+func (log *Logger) SetLevel(level Level) {
+	log.level = level
+	log.config.Level.SetLevel(level)
+}
+
+func SetLevel(level Level) {
+	logger.SetLevel(level)
+}
